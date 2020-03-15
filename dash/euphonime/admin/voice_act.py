8090cb35824d9151744257a58b36eb85bc26e6b5
liokta <shiroasasin@gmail.com>
2020-03-15 08:50:25 +0700

    Seiyuu Vioce

--- dash/euphonime/admin/voice_act.py
+++ dash/euphonime/admin/voice_act.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from euphonime.models import VoiceAct
+from euphonime.models import VoiceAct, SampleVoiceAct
+
+class SampleVoiceActInline(admin.TabularInline):
+    model = SampleVoiceAct
+    extra = 1
 
 @admin.register(VoiceAct, site=admin.site)
 class VoiceActAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
+
+    inlines = [
+        SampleVoiceActInline,
+    ]
--- dash/euphonime/models/voice_act.py
+++ dash/euphonime/models/voice_act.py
@@ -13,3 +13,15 @@ class VoiceAct(models.Model):
     class Meta:
         db_table = 'voice_acts'
         verbose_name = 'Voice Actress/Actor'
+
+
+class SampleVoiceAct(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
+    seiyuu = models.ForeignKey('VoiceAct', null=True, blank=True, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128, null=True, blank=True)
+    audio = models.FileField(upload_to='seiyuu/audio', max_length=100, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'voice_act_samples'
