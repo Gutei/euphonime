73e0f8ed8dc9bb27535a41bf1ab947f2959e8331
liokta <shiroasasin@gmail.com>
2020-03-17 10:40:25 +0700

    Celery init

--- dash/dash/__init__.py
+++ dash/dash/__init__.py
@@ -2,7 +2,8 @@ from __future__ import absolute_import, unicode_literals
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-DEBUG = False
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dash.settings_prod')
 from .celery import app as celery_app
 
 __all__ = ('celery_app',)
