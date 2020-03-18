0b5f750d36932d8a6e52c3e9b12a0c31d7ed8387
Gutei <anggeraryp@gmail.com>
2020-03-19 01:59:12 +0700

    add discord and url link

--- dash/euphonime/views/home.py
+++ dash/euphonime/views/home.py
@@ -9,6 +9,7 @@ def home(request):
     article = Article.objects.filter(is_publish=True).order_by('-updated')[:4]
     template_name = 'euphonime/home.html'
     this_sesason = Season.objects.filter(is_season=True).first()
+    polls = []
     anime_season = ses_anime
     if this_sesason:
         ses_anime = []
@@ -16,24 +17,20 @@ def home(request):
         for sa in anime_season:
             ses_anime.append(sa.anime)
 
-    polls = []
+        i = 1
 
-    i = 1
-
-    print(anime_season)
-
-    for s in anime_season:
-        score = UserAnimeScore.objects.filter(anime=s.anime).aggregate(Avg('score'))
-        polling = UserPolls.objects.filter(anime=s.anime).count()
-        watch = UserWatching.objects.filter(anime=s.anime).count()
-        polls.append({
-            'number': i,
-            'obj': s.anime,
-            'polls': polling if polling < 999999 else '1000000+',
-            'watch': watch if watch < 999999 else '1000000+',
-            'score': '-' if not score['score__avg'] else score['score__avg'],
-        })
-        i += 1
+        for s in anime_season:
+            score = UserAnimeScore.objects.filter(anime=s.anime).aggregate(Avg('score'))
+            polling = UserPolls.objects.filter(anime=s.anime).count()
+            watch = UserWatching.objects.filter(anime=s.anime).count()
+            polls.append({
+                'number': i,
+                'obj': s.anime,
+                'polls': polling if polling < 999999 else '1000000+',
+                'watch': watch if watch < 999999 else '1000000+',
+                'score': '-' if not score['score__avg'] else score['score__avg'],
+            })
+            i += 1
 
     context = {
         'season_nime': ses_anime,
