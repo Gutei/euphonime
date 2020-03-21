dd4bca05c18429ab66bc87d1cf49bd5385a5ed9c
liokta <shiroasasin@gmail.com>
2020-03-21 08:03:56 +0700

    Profile Pic

--- dash/euphonime/views/profile.py
+++ dash/euphonime/views/profile.py
@@ -29,7 +29,7 @@ def profile(request):
         image = social.get_avatar_url()
         logger.debug('===========GET PROFILE PIC {}============'.format(id))
         context = {
-            'profile_pic': image,
+            'profile_pic': str(image),
         }
 
     if not profile:
