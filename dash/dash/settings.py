61eed98703c04ebe60d75c3c3da1abc802e343a4
liokta <shiroasasin@gmail.com>
2020-03-23 03:39:24 +0700

    Releas Mordred

--- dash/dash/settings.py
+++ dash/dash/settings.py
@@ -237,8 +237,8 @@ ACCOUNT_DEFAULT_HTTP_PROTOCOL = "https"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com' #'smtp.gmail.com'
-# EMAIL_USE_TLS = True
-EMAIL_USE_SSL = True
-EMAIL_PORT = 465
+EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = True
+EMAIL_PORT = 587
 EMAIL_HOST_USER = 'projecteupho@gmail.com'
 EMAIL_HOST_PASSWORD = 'eupho2020'
