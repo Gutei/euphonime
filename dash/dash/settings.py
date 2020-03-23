6d40885d47c612fee352c64ac3733540135f82f3
liokta <shiroasasin@gmail.com>
2020-03-23 21:54:50 +0700

    Session cookies

--- dash/dash/settings.py
+++ dash/dash/settings.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django_cookies_samesite.middleware.CookiesSameSite',
+    # 'django_cookies_samesite.middleware.CookiesSameSite',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
