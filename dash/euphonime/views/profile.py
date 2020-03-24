a9d283ad74b2ef99cf971321d2fa5e8bda3a32a4
liokta <shiroasasin@gmail.com>
2020-03-25 01:49:20 +0700

    User profile threads

--- dash/euphonime/views/profile.py
+++ dash/euphonime/views/profile.py
@@ -64,10 +64,6 @@ def profile(request):
 
     context['biodata'] = escape_biod
 
-    # exeption user
-    if profile.user.email == "hixotow831@upcmaill.com":
-        context['biodata'] = biod
-
     watch_anime = UserWatching.objects.filter(user=profile)
 
     context['watch_data'] = [
